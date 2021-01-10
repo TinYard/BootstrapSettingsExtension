b3b50596be2f7919358bbbf9f233f824251264e0
Kieran Bond <kieran.bond@iress.com>
2021-01-10 18:01:47 +0000

    Add mappings to Extension
    Update dependencies

--- BootstrapSettingsExtension/BootstrapSettingsExtension.cs
+++ BootstrapSettingsExtension/BootstrapSettingsExtension.cs
@@ -1,4 +1,11 @@
 ﻿using TinYard.API.Interfaces;
+using TinYard.BootstrapSettings.API.Events;
+using TinYard.BootstrapSettings.API.Interfaces;
+using TinYard.BootstrapSettings.API.Mediators;
+using TinYard.BootstrapSettings.Impl.Commands;
+using TinYard.BootstrapSettings.Impl.Guards;
+using TinYard.Extensions.CommandSystem.API.Interfaces;
+using TinYard.Extensions.MediatorMap.API.Interfaces;
 
 namespace TinYard.BootstrapSettings
 {
@@ -11,6 +18,18 @@ namespace TinYard.BootstrapSettings
             _context = context;
 
             //Install any dependencies here
+            var mediatorMapper = _context.Mapper.GetMappingValue<IMediatorMapper>();
+            var commandMap = _context.Mapper.GetMappingValue<ICommandMap>();
+
+            //Map Mediators
+            mediatorMapper.Map<IBootstrapSettingReceiver>().ToMediator<BootstrapSettingReceiverMediator>();
+            mediatorMapper.Map<IBootstrapSettingRequestor>().ToMediator<BootstrapSettingRequestorMediator>();
+
+            //Map Commands
+            commandMap
+                .Map<RetrieveBootstrapSettingEvent>(RetrieveBootstrapSettingEvent.Type.Get)
+                .ToCommand<RetrieveBootstrapSettingCommand>().WithGuard<BootstrapSettingRequestGuard>();
+
         }
     }
 }
